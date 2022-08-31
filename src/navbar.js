4554fa9e9ca27d31fd09900e04d18e2f2b3a7f51
Arjun <arjunsingh8112@gmail.com>
2022-08-31 11:23:15 +0530

    added navbar

---
+++ src/navbar.js
@@ -0,0 +1,20 @@
+const { ContextReplacementPlugin } = require("webpack")
+
+const navbar = document.createElement('nav')
+const navLinks = document.createElement('ul')
+
+function createNavLink(txt, lnk) {
+    const li = document.createElement('li')
+    const link = document.createElement('a')
+    link.textContent = txt
+    link.setAttribute('href', `${lnk}`)
+    li.appendChild(link)
+    navLinks.appendChild('li')
+}
+
+createNavLink('Menu', 'menu')
+createNavLink('About Us', 'about-us')
+createNavLink('Contact', 'contact')
+
+navbar.appendChild(navLinks)
+content.appendChild(navbar)
