529fe8da92d3250535e486f65de090ec5bfc8d06
Arjun <arjunsingh8112@gmail.com>
2022-09-02 06:48:28 +0530

    changes links to btns in navbar. added ID to  menu.

--- src/navbar.js
+++ src/navbar.js
@@ -3,19 +3,21 @@ import bookButton from "./button.js";
 const navbar = document.createElement('nav')
 const navLinks = document.createElement('ul')
 
-function createNavLink(txt, lnk) {
+function createNavLink(txt, id) {
     const li = document.createElement('li')
     const btn = document.createElement('button')
     btn.textContent = txt
     // link.setAttribute('href', `${lnk}`)
-    btn.setAttribute('id', `${lnk}`)
+    btn.setAttribute('id', `${id}`)
     li.appendChild(btn)
     navLinks.appendChild(li)
 }
 
 createNavLink('Home', 'home')
-createNavLink('Menu', '')
+createNavLink('Menu', 'menu')
 createNavLink('About Us', 'about-us')
 bookButton(navLinks)
 navbar.appendChild(navLinks)
 content.appendChild(navbar)
+
+
