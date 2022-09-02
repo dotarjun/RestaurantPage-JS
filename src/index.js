9abd81b32d050d54a85a60aa2b5cbf519dd3693d
Arjun <arjunsingh8112@gmail.com>
2022-09-02 06:16:46 +0530

    tab logic

--- src/index.js
+++ src/index.js
@@ -1,29 +1,52 @@
 import './style.css';
+const siteContent = document.createElement('div')
+
 const content = document.createElement('div')
 content.setAttribute('id', 'content')
 
-document.body.appendChild(content);
+const menuContent = document.createElement('div')
+menuContent.setAttribute('id', 'menu-content')
+const menu = document.getElementById('menu')
+
+const aboutUsContent = document.createElement('div')
+aboutUsContent.setAttribute('id', 'about-us-content')
+
+
+let currentChild = content
+siteContent.appendChild(content)
+document.body.appendChild(siteContent);
+
+menu.addEventListener('click', () => {
+    siteContent.removeChild(currentChild)
+})
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+// footer
 const footer = document.createElement('footer')
-
 const socials = document.createElement('div')
 socials.classList.add('socials')
-
 function addSocial(id) {
     const icons = document.createElement('img')
     icons.setAttribute('id', id)
     const link = document.createElement('a')
     link.setAttribute('href', `https://www.${id}.com/dotarjun`)
     link.appendChild(icons)
-
     socials.appendChild(link)
 }
 addSocial('Github')
 addSocial('Twitter')
-
 footer.appendChild(socials)
-
 document.body.appendChild(footer)
