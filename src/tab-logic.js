9a71886e46f5f137f8ebe2fe881b3695e4cf2c43
Arjun <arjunsingh8112@gmail.com>
2022-09-02 07:17:17 +0530

    add event listener to each btn

--- src/tab-logic.js
+++ src/tab-logic.js
@@ -1,12 +1,20 @@
 const siteContent = document.getElementById('site-content')
 
-const menu = document.getElementById('menu')
+const menuButton = document.getElementById('menu')
+const homeButton = document.getElementById('home')
+const aboutUsButton = document.getElementById('about-us')
 let currentChild = content
 
-menu.addEventListener('click', () => {
-    console.log('CLICK')
-    siteContent.removeChild(currentChild)
-    currentChild = menu
-    
-})
+function tabSwitch(btn) {
+    btn.addEventListener('click', () => {
+        console.log('CLICK')
+        siteContent.removeChild(currentChild)
+        currentChild = btn
+    })
+}
+tabSwitch(menuButton)
+tabSwitch(homeButton)
+tabSwitch(aboutUsButton)
+
+
 
