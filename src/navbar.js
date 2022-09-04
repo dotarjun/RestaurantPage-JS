c78abc03bb367443c7dd802bec4cff9a892ee6a2
Arjun <arjunsingh8112@gmail.com>
2022-09-04 23:50:17 +0530

    👨‍👩‍👦  Change to pass a parent as a param over hard coding homeContent

--- src/navbar.js
+++ src/navbar.js
@@ -1,24 +1,29 @@
+
 import bookButton from "./button.js";
-const homeContent = document.getElementById('home-content')
 
-const navbar = document.createElement('nav')
-const navLinks = document.createElement('ul')
+export function addNavbar(parentEle) {
+    // const homeContent = document.getElementById('home-content')
 
-function createNavLink(txt, id) {
-    const li = document.createElement('li')
-    const btn = document.createElement('button')
-    btn.textContent = txt
-    // link.setAttribute('href', `${lnk}`)
-    btn.setAttribute('id', `${id}`)
-    li.appendChild(btn)
-    navLinks.appendChild(li)
+    const navbar = document.createElement('nav')
+    const navLinks = document.createElement('ul')
+
+    function createNavLink(txt, id) {
+        const li = document.createElement('li')
+        const btn = document.createElement('button')
+        btn.textContent = txt
+        // link.setAttribute('href', `${lnk}`)
+        btn.setAttribute('id', `${id}`)
+        li.appendChild(btn)
+        navLinks.appendChild(li)
+    }
+
+    createNavLink('Home', 'home')
+    createNavLink('Menu', 'menu')
+    createNavLink('About Us', 'about-us')
+    bookButton(navLinks)
+    navbar.appendChild(navLinks)
+    parentEle.appendChild(navbar)
 }
 
-createNavLink('Home', 'home')
-createNavLink('Menu', 'menu')
-createNavLink('About Us', 'about-us')
-bookButton(navLinks)
-navbar.appendChild(navLinks)
-homeContent.appendChild(navbar)
 
 
