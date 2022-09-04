0b7fbd9f90ce9e63b2c3a97a25e905ef0bfc9016
Arjun <arjunsingh8112@gmail.com>
2022-09-04 23:44:37 +0530

    import navbar,fixed nothing on click button bug, try removing childs from DOM (needs to be fixed)

--- src/tab-logic.js
+++ src/tab-logic.js
@@ -1,31 +1,45 @@
-import { addHomeContent } from "./home-main.js"
-// const { addHomeContent } = require("./home-main")
-
 const siteContent = document.getElementById('site-content')
 const homeContent = document.getElementById('home-content')
+const mainContent = document.createElement('main');
 
 
+import { addNavbar } from "./navbar.js"
+addNavbar(homeContent)
+
+import { addHomeContent } from "./home-main.js"
+addHomeContent()
 
 const homeButton = document.getElementById('home')
 const menuButton = document.getElementById('menu')
 const aboutUsButton = document.getElementById('about-us')
+
+
+
 let currentChild = homeContent
+let emptyChild = ''
 
 function tabSwitch(btn) {
     btn.addEventListener('click', () => {
-        console.log('CLICK')
-        siteContent.removeChild(currentChild)
-        currentChild = btn
-        if (currentChild == homeButton) {
+        const currentButton = btn
+        currentChild.replaceChildren(emptyChild)
+        if (currentButton == homeButton) {
+            const homeMainContent = document.querySelector('main');
+            homeMainContent.innerHTML = 'BRUHHH'
+            addNavbar(homeContent)
             addHomeContent();
-            console.log('addhomecontent');
-            console.log(currentChild);
+            siteContent.appendChild(currentChild)
         }
-        siteContent.appendChild(currentChild)
     })
 }
 
+function removeChildren(e) {
+    let child = e.lastElementChild;
+    while (child) {
+        e.removeChild(child);
+        child = e.lastElementChild;
 
+    }
+}
 
 tabSwitch(homeButton)
 tabSwitch(menuButton)
