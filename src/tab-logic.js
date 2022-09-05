f5b9ecf50e34dd208e8cd14f7d0b359ffca46068
Arjun <arjunsingh8112@gmail.com>
2022-09-05 12:13:27 +0530

    change approach by hiding/unhiding content divs rather than deleting and adding

--- src/tab-logic.js
+++ src/tab-logic.js
@@ -1,7 +1,8 @@
 const siteContent = document.getElementById('site-content')
 const homeContent = document.getElementById('home-content')
+const menuContent = document.getElementById('menu-content')
+const aboutUsContent = document.getElementById('about-us-content')
 const mainContent = document.getElementsByTagName('main');
-console.log(mainContent)
 
 import { addNavbar } from "./navbar.js"
 addNavbar(homeContent)
@@ -16,31 +17,45 @@ const aboutUsButton = document.getElementById('about-us')
 
 
 let currentChild = homeContent
-let emptyChild = ''
 
 function tabSwitch(btn) {
     btn.addEventListener('click', () => {
         const currentButton = btn
-        currentChild.replaceChildren(emptyChild)
         if (currentButton == homeButton) {
-            const homeMainContent = document.querySelector('main');
-            // homeMainContent.innerHTML = 'BRUHHH'
-            removeChildren(currentChild)
-            addNavbar(homeContent)
-            addHomeContent();
+            if (currentChild !== homeContent) {
+                addNavbar(homeContent)
+                addHomeContent();
+                siteContent.appendChild(currentChild)
+            }
+        }
+        if (currentButton == menuButton) {
+            // siteContent.removeChild(currentChild)
+            currentChild.style.display = 'none'
+            currentChild = menuContent
+            currentChild.style.display = 'initial'
             siteContent.appendChild(currentChild)
+            addNavbar(currentChild)
         }
     })
 }
 
-function removeChildren(e) {
-    let child = e.firstChild;
-    while (child.hasChildNodes) {
-        e.removeChild(child);
-        child = e.lastElementChild;
-        // You can take a look at removeChildNode, hasChildNodes, and firstChild
-    }
-}
+
+// function removeChildren(e) {
+//     let child = e.childNodes;
+//     console.log("🚀 ----------------------------------------------------------------🚀")
+//     console.log("🚀 ~ file: tab-logic.js ~ line 38 ~ removeChildren ~ child", child)
+//     console.log("🚀 ----------------------------------------------------------------🚀")
+//     if (child.length > 4) {
+//         for (let i = 1; i < 7; i++) {
+//             e.removeChild(child[i]);
+//         }
+//     }
+
+
+
+//     // You can take a look at removeChildNode, hasChildNodes, and firstChild
+// }
+
 
 
 
