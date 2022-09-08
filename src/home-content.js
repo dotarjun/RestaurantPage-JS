d516a7cbd7e02dfc063872adddd2b1c83b3a98f1
Arjun <arjunsingh8112@gmail.com>
2022-09-08 11:16:44 +0530

    sync all

--- src/home-content.js
+++ src/home-content.js
@@ -36,7 +36,7 @@ function addCover(msg) {
 }
 
 export function addHomeContent() {
-    addCover('Ratatouille Restaurant')
+    addCover()
     homeText('Remy (our lead chef) dreamt of becoming a great chef, despite being a rat in a rodent-phobic profession. Life led him to Paris to follow his dream, and with the help of hapless garbage boy Linguini he put his culinary skills to the test in the kitchen but he has to stay in hiding at the same time, with hilarious consequences. Remy eventually got the chance to prove his culinary abilities to THE Anton Ego, the great food critic. With a surprisingly positive review, we (formerly known as Gusteau\'s) were able to rise back to our old fame and rewrite history in French Cuisine.')
     homeText('But that would change soon...')
     homeText('Following Ego\'s review, we were ~ratted~ out. Thus Gustaeu\'s was shut down due to undisclosable reasons. But fear not, Gustaeu\'s spirit still lives on with our top of the notch chefs. With a highly diverse staff and an inclusonary kitchen open for individuals who love cooking, we have been rated Paris\'s best place to eat (and cook) at. At Ratatouille we embody Gustaeu\'s motto ')
@@ -45,7 +45,3 @@ export function addHomeContent() {
     bookButton(mainContent)
     homeContent.appendChild(mainContent)
 }
-
-// addHomeContent()
-
-
